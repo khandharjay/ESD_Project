8dbbc71f8bb99b06ddb095abf6a6f93b39730e27
smithakamat <smitha88kamat@gmail.com>
2013-05-03 11:27:19 -0600

    Commented the c_graph.c file

--- c_graph.c
+++ c_graph.c
@@ -1,16 +1,29 @@
+/*Description: This C file plots the titlt angle sensed by the gyroscope and accelerometer sensors and 
+displays it as function of time. The gnuplot utility has been used in order to plot the 
+graph.
+Authors: Smitha Sunil Kamat, Jay Khandhar
+*/
+
+/*Preprocessor directives*/
 #include <stdio.h>
 #include <stdlib.h>
 
 void main()
 {
 	int i =0;
+
+	/*Open the text file containing the titlt angles and time in read mode*/
 	FILE *temp = fopen("angle_data.txt", "r");
 	
-
+	/*Character pointer to the data in the text file accessible to gnuplot*/
 	char *commandsForGnuplot[] = {"set title \"Tilt angle measurement\"", "plot 'angle_data.txt'"};
-	FILE *gnuplotPipe = popen("gnuplot -persistent", "w");
-	printf("PLotting\n");
 
+	/*Create a pipe between gnuplot and the text file */
+	FILE *gnuplotPipe = popen("gnuplot -persistent", "w");
+
+	printf("Plotting the graph\n");
+
+	/*Plot the graph, tilt angle along Y axis and time along X axis*/
 	for (i =0; i<2;i++)
 	{
 		fprintf(gnuplotPipe, "%s \n", commandsForGnuplot[i]);
